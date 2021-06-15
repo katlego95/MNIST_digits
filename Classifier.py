41105c77572c2126c2606d78784947d734956bee
Katlego Gaveni <katlegobeta@gmail.com>
2021-06-15 17:13:45 +0200

    imported relevant classes and started to construct training data sets

--- Classifier.py
+++ Classifier.py
@@ -1,11 +1,27 @@
 import random
 import torch
-from torch import nm
+from torch.nn import nn
 import math
 import torchvision
-from torchvision import transforms, datasets 
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
 from Perceptron import Perceptron
 
 
+class NN(object):
+	"""docstring for NN"""
+	def __init__(self, arg, size, outputs):
+		super(NN, self).__init__()
+		self.fc1 = nn.Linear(size, 50)
+		self.fc2 = nn.Linear(50, outputs)
+
+	def forward(self, x):
+		x = F.relu(self.fc1(x))
+		x = self.fc2(x)
+		return x
+		
+
 if __name__ == '__main__':
+
+
 
