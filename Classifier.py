26e94c4394b3b1adf3f1f00b51f249b598d724c6
Katlego Gaveni <katlegobeta@gmail.com>
2021-06-15 18:13:21 +0200

    fixed dataset file path problem

--- Classifier.py
+++ Classifier.py
@@ -1,11 +1,12 @@
 import random
 import torch
-from torch.nn import nn
+import torch.nn as nn
 import math
 import torchvision
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 from Perceptron import Perceptron
+import os.path
 
 
 class NN(object):
@@ -29,13 +30,21 @@ if __name__ == '__main__':
 	batch_size = 40
 	hidden_layers = 100
 
+	path = "./data"
+ 
+# Expand an initial ~ component
+# in the given path
+# using os.path.expanduser() method
+	full_path = os.path.expanduser(path)
+
+
 	#GPU accelerators 
 	device = torch.device('cude' if torch.cuda.is_available() else 'cpu')
 
 	#load data fron Mnist training sets 
 
-	training_data = datasets.MNIST(root='./MNIST', train=True, transfrom=transfrom.ToTensor(), download=False)
-	test_data = datasets.MNIST(root="./MNIST", train=False,transfrom=transforms.ToTensor(), download=False)
+	training_data = datasets.MNIST(root=full_path, train=True, transform=transforms.ToTensor(), download=False)
+	test_data = datasets.MNIST(root=full_path, train=False,transform=transforms.ToTensor(), download=False)
 
 	training_loader = torch.utils.data.DataLoader(dataset=training_data, batch_size=batch_size,shuffle=True, num_workers=2)
 	test_loader = torch.utils.data.DataLoader(dataset=test_data, batch_size=batch_size,shuffle=False, num_workers=2)
