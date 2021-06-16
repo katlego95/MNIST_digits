e2f9f930efce73a64a41c7798f5f47f7f5cfff7c
Katlego Gaveni <katlegobeta@gmail.com>
2021-06-16 14:28:45 +0200

    rethinking how random data set i geneerated

--- XOR.py
+++ XOR.py
@@ -44,8 +44,12 @@ if __name__ == '__main__':
 
 		for i in range(num_train):
 			training_examples.append([random.random(), random.random()])
+			if (training_examples[i][0] >= 0.75 and training_examples[i][1] >= 0.75): 
+				t_label=0.0
+			else:
+				t_label=1.0
 			# We want our perceptron to be noise tolerant, so we label all examples where x1 and x2 > 0.8 as 1.0
-			training_labels.append(0.0 if training_examples[i][0] > 0.75 and training_examples[i][1] > 0.75 else 1.0)
+			training_labels.append(t_label)
 
 	if generate_validation_set:
 		validate_examples = []
@@ -53,33 +57,63 @@ if __name__ == '__main__':
 
 		for i in range(num_train):
 			validate_examples.append([random.random(), random.random()])
-			validate_labels.append(0.0 if validate_examples[i][0] > 0.75 and validate_examples[i][1] > 0.75 else 0.0)
+			if (validate_examples[i][0] >= 0.75 and validate_examples[i][1] >= 0.75): 
+				v_label=0.0
+			else:
+				v_label=1.0
+			validate_labels.append(v_label)
+
+	test_d = []
+	validate_d = []
+
+	for i in range(num_train):
+
+		train_d=(training_examples[i][0],training_examples[i][1],training_labels[i])
+		vali_d=(validate_examples[i][0],validate_examples[i][1],validate_labels[i])
+
+		test_d.append(train_d)
+		validate_d.append(vali_d)
+
+	print('training data')
+	for i in range(num_train):
+		print(test_d[i])
+	print('------------------------------------------------------')
+	print('validation data')
+	for i in range(num_train):
+		print(validate_d[i])
+	print('------------------------------------------------------')
+	
 
 
 	# Create Perceptron
-	NAND1 = Perceptron(2, bias=-0.75)
-	NAND2 = Perceptron(2, bias=-0.75)
-	NAND3 = Perceptron(2, bias=-0.75)
-	NAND4 = Perceptron(2, bias=-0.75)
+	NAND1 = Perceptron(2, bias=-1.5)
+	NAND2 = Perceptron(2, bias=-1.5)
+	NAND3 = Perceptron(2, bias=-1.5)
+	NAND4 = Perceptron(2, bias=-1.5)
 
 	done = False
 	input1= None
+	learning_rate = 0.2
 
 	#print(NAND1.weights)
-	valid_percentage1 = NAND1.validate(validate_examples, validate_labels, verbose=True)
-	#print(valid_percentage1)
+	valid_percentage1 = NAND1.validate(validate_examples, validate_labels, verbose=False)
 
 	#print(NAND2.weights)
-	valid_percentage2 = NAND2.validate(validate_examples, validate_labels, verbose=True)
-	#print(valid_percentage1)
+	valid_percentage2 = NAND2.validate(validate_examples, validate_labels, verbose=False)
+	
 
 	#print(NAND3.weights)
-	valid_percentage3 = NAND3.validate(validate_examples, validate_labels, verbose=True)
-	#print(valid_percentage1)
+	valid_percentage3 = NAND3.validate(validate_examples, validate_labels, verbose=False)
+	
 
 	#print(NAND4.weights)
-	valid_percentage4 = NAND4.validate(validate_examples, validate_labels, verbose=True)
-	#print(valid_percentage1)
+	valid_percentage4 = NAND4.validate(validate_examples, validate_labels, verbose=False)
+	
+
+	print('NAND GATE 1 accuracy : ' +str(valid_percentage1))
+	print('NAND GATE 2 accuracy : ' +str(valid_percentage2))
+	print('NAND GATE 3 accuracy : ' +str(valid_percentage3))
+	print('NAND GATE 4 accuracy : ' +str(valid_percentage4))
 
 	i1 = 0
 	i2 = 0
@@ -89,45 +123,47 @@ if __name__ == '__main__':
 		print('Training NAND GATE_1...')
 		while valid_percentage1 < 0.98:
 			i1 += 1
-			NAND1.train(training_examples, training_labels, 0.2)  # Train our Perceptron
-			valid_percentage = NAND1.validate(validate_examples, validate_labels, verbose=True) # Validate it
-			print(valid_percentage)
-			if i1 == 1000: 
+			NAND1.train(training_examples, training_labels, learning_rate)  # Train our Perceptron
+			valid_percentage1 = NAND1.validate(validate_examples, validate_labels, verbose=False) # Validate it
+			if i1 == 10000: 
 				break
 		print('------------------------------------------------------')
 		print('Training NAND GATE_2...')
 		while valid_percentage2 < 0.98:
 			i2 += 1
-			NAND2.train(training_examples, training_labels, 0.2)  # Train our Perceptron
-			valid_percentage = NAND2.validate(validate_examples, validate_labels, verbose=True) # Validate it
-			print(valid_percentage)
-			if i2 == 1000: 
+			NAND2.train(training_examples, training_labels, learning_rate)  # Train our Perceptron
+			valid_percentage2 = NAND2.validate(validate_examples, validate_labels, verbose=False) # Validate it
+			if i2 == 10000:
 				break
 		print('------------------------------------------------------')
 		print('Training NAND GATE_3...')
 		while valid_percentage3 < 0.98:
 			i3 += 1
-			NAND3.train(training_examples, training_labels, 0.2)  # Train our Perceptron
-			valid_percentage = NAND3.validate(validate_examples, validate_labels, verbose=True) # Validate it
-			print(valid_percentage)
-			if i3 == 1000: 
+			NAND3.train(training_examples, training_labels, learning_rate)  # Train our Perceptron
+			valid_percentage3 = NAND3.validate(validate_examples, validate_labels, verbose=False) # Validate it
+			if i3 == 10000: 
 				break
 		print('------------------------------------------------------')
 		print('Training NAND GATE_4...')
 		while valid_percentage4 < 0.98:
 			i4 += 1
-			NAND4.train(training_examples, training_labels, 0.2)  # Train our Perceptron
-			valid_percentage = NAND4.validate(validate_examples, validate_labels, verbose=True) # Validate it
-			print(valid_percentage)
-			if i4 == 1000: 
+			NAND4.train(training_examples, training_labels, learning_rate)  # Train our Perceptron
+			valid_percentage4 = NAND4.validate(validate_examples, validate_labels, verbose=False) # Validate it
+			if i4 == 10000: 
 				break
-		print('Constructing Network...')
 		print('Done!')
 		print('------------------------------------------------------')
 		done = True
 		# This is just to break the training if it takes over 50 iterations. (For demonstration purposes)
 		# You shouldn't need to do this as your networks may require much longer to train. 
 		
+
+	print('NAND GATE 1 accuracy after training : ' +str(valid_percentage1))
+	print('NAND GATE 2 accuracy after training : ' +str(valid_percentage2))
+	print('NAND GATE 3 accuracy after training : ' +str(valid_percentage3))
+	print('NAND GATE 4 accuracy after training : ' +str(valid_percentage4))
+	print('Constructing Network...')
+	print('------------------------------------------------------')
 	print('Please enter two inputs: ')
 	print('type exit to terminate programme')
 
